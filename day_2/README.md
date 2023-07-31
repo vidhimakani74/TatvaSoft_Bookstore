4ae46975a68aca9f3869833bc1f0f976fae473d8
Vidhi Makani <vidhimakani007@gmail.com>
2023-07-31 20:33:59 +0530

    notes on default export

--- day_2/README.md
+++ day_2/README.md
@@ -1,4 +1,8 @@
-# Getting Started with Create React App
+# Day 2 Task submission
+
+![User component](image.png)
+
+## Getting Started with Create React App
 
 This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
 
--- day_2/src/User.js
+++ day_2/src/User.js
@@ -3,3 +3,19 @@
         <h1>This is User componenet</h1>
     )
  };
+
+
+//  or
+
+//  const User=()=>{
+//     return(
+//         <h1>This is User componenet</h1>
+//     )
+//  };
+
+//  export {User};
+
+// if you want to export component without {} then user "default" keyword..
+// with default keyword u can export only one var/ fun
+// export default User;
+// & don't use {} in import as well & u can use any name in import
