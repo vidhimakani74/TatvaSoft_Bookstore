9904ae4fa308b784a23ce08d69801366139ae8e5
Vidhi Makani <vidhimakani007@gmail.com>
2023-08-02 20:23:51 +0530

    add button from material UI

--- day_3/src/components/profile/Profile.jsx
+++ day_3/src/components/profile/Profile.jsx
@@ -1,10 +1,14 @@
-
 import { NavLink } from "react-router-dom";
+import classes from "./Profile.module.css"
+import {Button} from "@material-ui/core" 
 export const Profile=()=>{
     return(
         <>
-        <h1>Profile component</h1>
+        <h1 className={classes.heading}>Profile component</h1>
         <h1><NavLink to={"/"}>Home</NavLink></h1>
+        <h1><Button variant="contained" color="primary" size="large" className="clk-btn" classes={{
+            
+        }}>Click Me</Button></h1>
         </>
     )
 };
