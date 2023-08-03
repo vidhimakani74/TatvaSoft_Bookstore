cf081582dc5f6708ab2c5050d767a06521d85c0a
Vidhi Makani <vidhimakani007@gmail.com>
2023-08-03 21:55:26 +0530

    add user component

---
+++ day_4/src/User.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import { NavLink } from "react-router-dom";
+// import logo from "../src//assets/logo.svg"
+// import { Profile } from './components/profile/Profile';
+
+export const User=({name, age})=>{
+    return(
+        // only return 1 element so that when u want to use multiple div or any other element 
+        // then u can wrap it into the <></> or <React.Fragment></React.Fragment> tags
+
+        // this is optional -> u can use <></> this also
+        <React.Fragment> 
+            <h1>This is User component
+                <h4>Welcome {name}</h4>
+                <h6>Age {age}</h6>
+
+                <NavLink to={"/"}>Home</NavLink>
+            </h1>
+            <h2>
+                Go to Home 👆
+            </h2>
+            {/* <img src={logo} /> */}
+        </React.Fragment>
+    )
+ };
+
+export const Home=()=>{
+    return(
+        <>
+        <h1>This is Home component
+            <h4><NavLink to={"/User"}>User</NavLink></h4>
+            <h4><NavLink to={"/Profile"}>Profile</NavLink></h4>
+        </h1>
+        </>
+
+    )
+ };
+
